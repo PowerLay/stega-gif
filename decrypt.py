9218410a63a7a4b64e92601f8993a362a819ad7e
PowerLay <1854pro@gmail.com>
2022-05-05 14:33:14 +0300

    Переделал под работу с gif

--- decrypt.py
+++ decrypt.py
@@ -1,23 +1,34 @@
-from PIL import Image 		
+from PIL import Image
 from re import findall
 
+
 def stega_decrypt():
-	
-	a = []						    
-	keys = []
-	img = Image.open(input("path to image: "))				
-	pix = img.load()
-	f = open(input('path to keys: '),'r')
-	y = str([line.strip() for line in f])				
-															
-	for i in range(len(findall(r'\((\d+)\,',y))):
-		keys.append((int(findall(r'\((\d+)\,',y)[i]),int(findall(r'\,\s(\d+)\)',y)[i]))) 	
-	for key in keys:
-		a.append(pix[tuple(key)][0])							
-	return ''.join([chr(elem) for elem in a])	
+
+    a = []
+    keys = []
+    img = Image.open(input("path to image: "))
+    f = open(input('path to keys: '), 'r')
+    y = str([line.strip() for line in f])
+
+    for i in range(len(findall(r'\d+\s\((\d+)\,', y))):
+        coords = (
+            (
+                int(findall(r'\((\d+)\,', y)[i]),
+                int(findall(r'\,\s(\d+)\)', y)[i])
+            )
+        )
+        frame = int(findall(r'(\d+)\s\(', y)[i])
+        keys.append((frame, coords))
+
+    for key in keys:
+        print(key)
+
+        frame, coords = key
+        img.seek(frame)
+        pix = img.load()
+        a.append(pix[coords][0])
+
+    return ''.join([chr(elem) for elem in a])
 
 
-print("you message: ",stega_decrypt())
-
-
-
+print("you message: ", stega_decrypt())
--- encrypt.py
+++ encrypt.py
@@ -1,25 +1,47 @@
-from PIL import Image, ImageDraw 
-from random import randint		
+from PIL import Image, ImageDraw, ImageSequence
+from random import randint
+import io
 
 
-def stega_encrypt():	
-	
-	keys = [] 					#сюда будут помещены ключи
-	img = Image.open(input("path to image: ")) 	#создаём объект изображения
-	draw = ImageDraw.Draw(img)	   		#объект рисования
-	width = img.size[0]  		   		#ширина
-	height = img.size[1]		   		#высота	
-	pix = img.load()				#все пиксели тут
-	f = open('keys.txt','w')			#текстовый файл для ключей
+def stega_encrypt():
 
-	for elem in ([ord(elem) for elem in input("text here: ")]):
-		key = (randint(1,width-10),randint(1,height-10))		
-		g, b = pix[key][1:3]
-		draw.point(key, (elem,g , b))														
-		f.write(str(key)+'\n')								
-	
-	print('keys were written to the keys.txt file')
-	img.save("newimage.png", "PNG")
-	f.close()
-												
+    keys = []  # сюда будут помещены ключи
+    frames = []  # массив кадров
+    # создаём объект изображения
+    img = Image.open(input("path to image: "))  # создаём объект изображения
+    width = img.size[0]  # ширина
+    height = img.size[1]  # высота
+    f = open('keys.txt', 'w')  # текстовый файл для ключей
+
+    for frame in ImageSequence.Iterator(img):
+        b = io.BytesIO()
+        frame.save(b, format="GIF")
+        frame = Image.open(b)
+        frames.append(frame)
+
+    for elem in ([ord(elem) for elem in input("text here: ")]):
+        print(elem)
+        coords = (randint(1, width-10), randint(1, height-10))
+        frame_id = randint(1, img.n_frames-1)
+
+        img.seek(frame_id)
+
+        pix = img.load()  # все пиксели тут
+        draw = ImageDraw.Draw(frames[frame_id])  # объект рисования
+
+        g, b = pix[coords][1:3]
+        draw.point(coords, (elem, g, b))
+
+        b = io.BytesIO()
+        frames[frame_id].save(b, format="GIF")
+        frame = Image.open(b)
+        frames[frame_id] = frame
+
+        f.write(str(frame_id) + " " + str(coords)+'\n')
+
+    print('keys were written to the keys.txt file')
+    frames[0].save('newimage.gif', save_all=True, append_images=frames[1:])
+    f.close()
+
+
 stega_encrypt()
